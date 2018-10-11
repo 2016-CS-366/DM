aa1db8ec89e032bdd8106986b9b58cd96f00bf64
2016-CS-366 <38296340+2016-CS-366@users.noreply.github.com>
2018-10-11 10:26:30 +0500

    miniphonebook

---
+++ Models/PersonViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MiniPhoneBook.Models
+{
+    public class PersonViewModel
+    {
+        public int PersonId { get; set; }
+        public string FirstName { get; set; }
+        public int MiddleName { get; set; }
+        public int LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public DateTime AddedOn { get; set; }
+        public string  AddedBy{ get; set; }
+        public string FaceBookId { get; set; }
+        public string HomeAddress { get; set; }
+        public string HomeCity { get; set; }
+        public string LinkedInId { get; set; }
+
+        public DateTime UpdateOn { get; set; }
+
+        public string TwitterId { get; set; }
+        public string EmailId { get; set; }
+
+
+
+
+
+
+
+    }
+}
---
+++ Models/contactViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MiniPhoneBook.Models
+{
+    public class contactViewModel
+    {
+        public int contactId { get; set; }
+        public string ContactNumber { get; set; }
+        public string Type { get; set; }
+        public int PersonId { get; set; }
+    }
+}
